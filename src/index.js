d5473f74b8402f765e30bf6364d9f81bad0783bd
sionelt <sltafengatoto@gmail.com>
2018-10-31 23:02:31 -0600

    comply with eslint and prettier

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 var Fetch = (function() {
   function _parseResponse(response, type) {
-    switch(type) {
+    switch (type) {
       case 'text':
-        return response.text();
+        return response.text()
       case 'json':
-        return response.json();
+        return response.json()
       case 'blob':
-        return response.blob();
+        return response.blob()
       case 'formData':
-        return response.formData();
+        return response.formData()
       case 'arrayBuffer':
-        return response.arrayBuffer();
+        return response.arrayBuffer()
     }
   }
 
@@ -18,72 +18,76 @@ var Fetch = (function() {
     return promise
       .then(response => {
         if (response.ok) {
-          return { promise, ok: true };
+          return {promise, ok: true}
         }
-        throw Error(response.status);
+        throw Error(response.status)
       })
       .catch(error => {
-        const { message } = error;
+        const {message} = error
         return {
-          error: { name: 'HttpError', message },
-          ok: false
-        };
-      });
+          error: {name: 'HttpError', message},
+          ok: false,
+        }
+      })
   }
 
   function _handleStandardErrors(error) {
-    const { name, message } = error;
+    const {name, message} = error
     return {
-      error: { name, message },
-      ok: false
-    };
+      error: {name, message},
+      ok: false,
+    }
   }
 
-  const Public = {};
+  const Public = {}
 
   Public.get = async function(url, option = {}, resType = 'json') {
-    const { ok, promise, error } = await _handleHttpErrors(fetch(url, option));
+    const {ok, promise, error} = await _handleHttpErrors(fetch(url, option))
     if (ok) {
       return promise
         .then(response => _parseResponse(response, resType))
-        .then(data => ({ data, ok: true }))
-        .catch(_handleStandardErrors);
+        .then(data => ({data, ok: true}))
+        .catch(_handleStandardErrors)
     }
-    return { error, ok: false };
-  };
+    return {error, ok: false}
+  }
 
   Public.post = async function(url, option, resType = 'json') {
-    const { ok, promise, error } = await _handleHttpErrors(fetch(url, option));
+    const {ok, promise, error} = await _handleHttpErrors(fetch(url, option))
     if (ok) {
       return promise
         .then(response => _parseResponse(response, resType))
-        .then(data => ({ data, ok: true }))
-        .catch(_handleStandardErrors);
+        .then(data => ({data, ok: true}))
+        .catch(_handleStandardErrors)
     }
-    return { error, ok: false };
-  };
+    return {error, ok: false}
+  }
 
   Public.put = async function(url, option, resType = 'text') {
-    const { ok, promise, error } = await _handleHttpErrors(fetch(url, option));
+    const {ok, promise, error} = await _handleHttpErrors(fetch(url, option))
     if (ok) {
       return promise
         .then(response => _parseResponse(response, resType))
-        .then(data => ({ data, ok: true }))
-        .catch(_handleStandardErrors);
+        .then(data => ({data, ok: true}))
+        .catch(_handleStandardErrors)
     }
-    return { error, ok: false };
-  };
+    return {error, ok: false}
+  }
 
-  Public.delete = async function(url, option = { method: 'delete' }, resType = 'text') {
-    const { ok, promise, error } = await _handleHttpErrors(fetch(url, option));
+  Public.delete = async function(
+    url,
+    option = {method: 'delete'},
+    resType = 'text',
+  ) {
+    const {ok, promise, error} = await _handleHttpErrors(fetch(url, option))
     if (ok) {
       return promise
         .then(response => _parseResponse(response, resType))
-        .then(data => ({ data, ok: true }))
-        .catch(_handleStandardErrors);
+        .then(data => ({data, ok: true}))
+        .catch(_handleStandardErrors)
     }
-    return { error, ok: false };
-  };
+    return {error, ok: false}
+  }
 
-  return Public;
-})();
+  return Public
+})()
